328ea18864b0d9c76a1df9e02c41e471649e22c6
tahersm <mandapwalataher@gmail.com>
2019-04-09 14:32:46 +0530

    updated Homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:login_page/Notification_.dart';
 import 'package:login_page/userprofile.dart';
 import 'naviRoute.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 
 class HomePage extends StatelessWidget {
   @override
@@ -21,8 +25,8 @@ class HomePage extends StatelessWidget {
         child: new ListView(
           children: <Widget>[
             new UserAccountsDrawerHeader(
-              accountName: new Text('Vidisha Shah'),
-              accountEmail: new Text('v@gmail.com'),
+              accountName: new Text('Taher M'),
+              accountEmail: new Text('t@gmail.com'),
               currentAccountPicture: new CircleAvatar(backgroundColor: Colors.black26,child: new Text('V'),),
               decoration: new BoxDecoration(color: Colors.blue[300]),
             ),
@@ -40,8 +44,6 @@ class HomePage extends StatelessWidget {
       ),
 
       body: feed(),
-
-
     );
   }
 }
@@ -52,14 +54,73 @@ class feed extends StatefulWidget {
 }
 
 class _feedState extends State<feed> {
+Future<List<Project>> _getFeeds() async{
+    List<Project> up = [];
+    var data = await http.get('https://jsonplaceholder.typicode.com/posts');
+    var jsonData = json.decode(data.body);
+
+    for(var u in jsonData){
+      //Project temp = Project(u["id"], u["name"], u["username"], u["email"]);
+      Project temp = Project(u["title"], u["body"]);
+      up.add(temp);
+    }
+
+    print(up.length);
+    return up;
+}
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Feeds"),
+      body: Container(
+        child: FutureBuilder(
+          future: _getFeeds(),
+          builder: (BuildContext context, AsyncSnapshot snapshot){
+            if(snapshot.data == null){
+              return Container(
+                child: Center(
+                  child: Text("Loading..."),
+                ),
+              );
+            }
+            else{
+              return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index){
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage("url"),
+                  ),
+                  title: Text(snapshot.data[index].title),
+                  subtitle: Text(snapshot.data[index].body),
+                  
+                );
+              },
+            );
+            }
+          },
+        ),
       ),
-
-
     );
   }
 }
+
+
+class Updates{
+  
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  
+  Updates(this.id, this.name, this.username, this.email);
+}
+
+class Project{
+  final String title;
+  final String body;
+
+  Project(this.title, this.body); 
+}
+
