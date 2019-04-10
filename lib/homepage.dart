2af3807370aa5b4075f9f9f39dc25d310f656aba
Manisbhai <shahvidisha1996@gmail.com>
2019-04-10 22:17:17 +0530

    Solving Conflicts

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,11 +4,7 @@ import 'package:login_page/userprofile.dart';
 import 'naviRoute.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 class HomePage extends StatefulWidget {
-
-
-
   @override
   HomePageState createState() {
     return new HomePageState();
@@ -16,16 +12,13 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-
   SharedPreferences prefs;
 
   var userid;
 
-
-    getdata() async{
+  getdata() async {
     prefs = await SharedPreferences.getInstance();
     userid = prefs.getString("userid");
-
   }
 
   @override
@@ -41,54 +34,66 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text(userid),
+        title: new Text('Home Page'),
         actions: <Widget>[
           new IconButton(icon: new Icon(Icons.search), onPressed: null),
-          new IconButton(icon: new Icon(Icons.notifications), onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => Notify()));
-          })
+          new IconButton(
+              icon: new Icon(Icons.notifications),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => Notify()));
+              })
         ],
       ),
-
       drawer: new Drawer(
         child: new ListView(
           children: <Widget>[
             new UserAccountsDrawerHeader(
               accountName: new Text('Vidisha Shah'),
               accountEmail: new Text('v@gmail.com'),
-              currentAccountPicture: new CircleAvatar(backgroundColor: Colors.black26,child: new Text('V'),),
+              currentAccountPicture: new CircleAvatar(
+                backgroundColor: Colors.black26,
+                child: new Text('V'),
+              ),
               decoration: new BoxDecoration(color: Colors.blue[300]),
             ),
-
-            new ListTile(title: new Text('Page 1'),
+            new ListTile(
+                title: new Text('Page 1'),
                 trailing: new Icon(Icons.arrow_forward),
-                onTap: ()=> Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext)=>UserProfile()))),
-            new ListTile(title: new Text('Page 2'),
+                onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+                    builder: (BuildContext) => UserProfile()))),
+            new ListTile(
+                title: new Text('Page 2'),
                 trailing: new Icon(Icons.arrow_forward),
-                onTap: ()=> Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext)=>new op('Page 2')))
-            ),
-            new ListTile(title: new Text('close'),trailing: new Icon(Icons.arrow_forward),onTap: (){Navigator.pop(context);}),
+                onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+                    builder: (BuildContext) => new op('Page 2')))),
+            new ListTile(
+                title: new Text('close'),
+                trailing: new Icon(Icons.arrow_forward),
+                onTap: () {
+                  Navigator.pop(context);
+                }),
           ],
         ),
       ),
-
       body: cardContain(),
-
-
     );
   }
 }
 
 class cardContain extends StatelessWidget {
   @override
-  Widget build_card(BuildContext context,int index) {
+  Widget build_card(BuildContext context, int index) {
     return Center(
-
       child: Card(
         elevation: 50,
         child: InkWell(
           splashColor: Colors.blue.withAlpha(30),
-          onTap: () { /* ... */ },
+          onTap: () {
+            /* ... */
+          },
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
@@ -102,7 +107,7 @@ class cardContain extends StatelessWidget {
                   children: <Widget>[
                     FlatButton(
                       child: const Text('Like'),
-                      onPressed: (){},
+                      onPressed: () {},
                     )
                   ],
                 ),
@@ -116,6 +121,6 @@ class cardContain extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: build_card,itemCount: 20);
+    return ListView.builder(itemBuilder: build_card, itemCount: 20);
   }
 }
